35e78e910d7736850ee8f270d71943626c7f154e
Jhoan Oswaldo Ome Vega <Jhoan-Oswaldo.Ome@endava.com>
2024-08-27 10:20:45 -0500

    Change the folder

--- To-Do-App/app/spec/persistence/sqlite.spec.js
+++ To-Do-App/app/spec/persistence/sqlite.spec.js
@@ -1,6 +1,6 @@
 const db = require('../../src/persistence/sqlite');
 const fs = require('fs');
-const location = process.env.SQLITE_DB_LOCATION || '/etc/todos/todo.db';
+const location = process.env.SQLITE_DB_LOCATION || './todo.db';
 
 const ITEM = {
     id: '7aef3d7c-d301-4846-8358-2a91ec9d6be3',
